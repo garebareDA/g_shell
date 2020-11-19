ef7ef408a54772b74c8e495ba83680b389a9ff99
garebare <garedaiki@gmail.com>
2020-11-19 10:24:55 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use termion::*;
+use termion::event::{Key, Event, MouseEvent};
+use termion::raw::IntoRawMode;
+use std::io::{Write, stdout, stdin};
+
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+        let mut line = String::new();
+        stdin().read_line(&mut line).expect("Faild to read line");
+        print!("{}", line);
+    }
 }
