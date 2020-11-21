d583da4747563a92d3c883ae670966effdbdf333
garebare <garedaiki@gmail.com>
2020-11-21 09:13:42 +0000

    add:exit

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ fn main() {
     loop {
         print!("{}@{}:{} > ",whoami::username(), whoami::hostname(), env::current_dir().unwrap().display());
         stdout().flush().unwrap();
+
         let mut line = String::new();
         stdin().read_line(&mut line).expect("Faild to read line");
+        line.remove(line.len() - 1);
+        let line_split:Vec<&str> = line.split(" ").collect();
+
+        if line_split[0] == "exit" {
+            return;
+        }
     }
 }
