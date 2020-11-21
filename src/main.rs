ba8071c426f03161844faaf6d3785f612d6d4119
garebare <garedaiki@gmail.com>
2020-11-21 09:01:55 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use termion::*;
-use termion::event::{Key, Event, MouseEvent};
-use termion::raw::IntoRawMode;
+use whoami;
+
 use std::io::{Write, stdout, stdin};
+use std::env;
 
 fn main() {
 
     loop {
+        print!("{}@{}:{} > ",whoami::username(), whoami::hostname(), env::current_dir().unwrap().display());
+        stdout().flush().unwrap();
         let mut line = String::new();
         stdin().read_line(&mut line).expect("Faild to read line");
-        print!("{}", line);
     }
 }
