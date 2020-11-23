90fac01f085709979b4e5dcd53f29d200e6eea55
garebare <garedaiki@gmail.com>
2020-11-23 07:15:22 +0000

    add:builtincommand

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ use std::env;
 use std::io::{stdin, stdout, Write};
 use std::process::exit;
 
-use nix::sys;
 use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
 use nix::unistd::*;
-
-use dirs;
 use whoami;
+
+use g_shell::built_in_command;
 
 fn main() {
     loop {
@@ -23,11 +22,6 @@ fn main() {
         stdin().read_line(&mut line).expect("Faild to read line");
         line.remove(line.len() - 1);
         let line_split: Vec<&str> = line.split(" ").collect();
-
-        if line_split[0] == "exit" {
-            return;
-        }
-
         match argvs_execute(&line_split) {
             Ok(()) => {}
             Err(e) => {
@@ -38,60 +32,67 @@ fn main() {
 }
 
 fn argvs_execute(argvs: &Vec<&str>) -> Result<(), String> {
-    match sh_launch() {
+    match built_in_command::cd::run_cd(argvs) {
         Ok(_) => {}
         Err(e) => {
             return Err(e);
         }
     }
 
-    if argvs[0] == "cd" {
-        if argvs.len() != 2 {
-            env::set_current_dir(dirs::home_dir().unwrap()).unwrap();
-            return Ok(());
-        } else if !env::set_current_dir(argvs[1]).is_ok() {
-            return Err(format!("cd {}: No such file or directory\n", argvs[1]));
+    match built_in_command::exit::run_exit(argvs) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(e);
+        }
+    }
+
+    match sh_launch() {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(e);
         }
     }
     return Ok(());
 }
 
 fn sh_launch() -> Result<(), String> {
+    //子プロセスの生成
     match unsafe { fork() } {
+        //親プロセス
         Ok(ForkResult::Parent { child, .. }) => {
             let pid: i32 = child.into();
+
+            //プロセスが起動できていなければエラー
             if pid == 0 {
                 return Err(format!("Error process"));
             } else if pid < 0 {
                 return Err(format!("Error forking"));
             } else {
-                loop {
-                    match waitpid(child, Some(WaitPidFlag::WUNTRACED)) {
-                        Ok(status) => match status {
-                            WaitStatus::Exited(_, _) => {
-                                println!("Exited");
-                                break;
-                            }
-
-                            WaitStatus::Stopped(_, _) => {
-                                println!("Stopped");
-                                break;
-                            }
-                            _ => {
-                                return Err(format!("Waiprocess EOF"));
-                            }
-                        },
-                        Err(_) => {
-                            return Err(format!("Waitprocess error"));
+                //子プロセスを待つ
+                match waitpid(child, Some(WaitPidFlag::WUNTRACED)) {
+                    Ok(status) => match status {
+                        WaitStatus::Exited(_, _) => {
+                            println!("Exited");
                         }
+
+                        WaitStatus::Stopped(_, _) => {
+                            println!("Stopped");
+                        }
+                        _ => {
+                            return Err(format!("Waiprocess EOF"));
+                        }
+                    },
+                    Err(_) => {
+                        return Err(format!("Waitprocess error"));
                     }
                 }
             }
         }
+        //子プロセス
         Ok(ForkResult::Child) => {
-            println!("I'm a new child process");
             exit(1);
-        },
+        }
+
         Err(_) => {
             return Err(format!("Fork Failed"));
         }
