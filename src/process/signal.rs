f9ab6c163bef7f8289c7a4fb6a590453ad7f7b79
garebare <garedaiki@gmail.com>
2020-11-30 13:18:56 +0000

    add:signal

---
+++ src/process/signal.rs
@@ -0,0 +1,15 @@
+use chan;
+use nix::sys::signal;
+use nix::sys::signal::SaFlags;
+use nix::sys::signal::{sigaction, SigAction, SigHandler, SigSet};
+
+extern "C" fn handle_signal(signum: i32) {}
+
+pub fn signal_action() {
+  let sa = SigAction::new(
+    SigHandler::Handler(handle_signal),
+    SaFlags::SA_RESETHAND,
+    SigSet::empty(),
+  );
+  unsafe { sigaction(signal::SIGINT, &sa) }.unwrap();
+}
